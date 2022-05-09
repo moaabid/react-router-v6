403cf6b98e79fa0e074dcfffb194bee003eaa1ff
moaabid <moaabid153@gmail.com>
2022-05-09 09:32:47 +0530

    side nav

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,26 +1,68 @@
-import { Outlet, Link } from 'react-router-dom'
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom'
 import { getInvoices } from '../data'
+import { TextInput, Navbar, UnstyledButton, Text, Space } from '@mantine/core'
 
 export default function Invoices() {
   let invoices = getInvoices()
+  let [searchParams, setSearchParams] = useSearchParams()
   return (
     <div style={{ display: 'flex' }}>
-      <nav
-        style={{
-          borderRight: 'solid 1px',
-          padding: '1rem',
-        }}
-      >
-        {invoices.map((invoice) => (
-          <Link
-            style={{ display: 'block', margin: '1rem 0' }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </Link>
-        ))}
-      </nav>
+      <Navbar width={{ base: 300 }} height={500} p="xs">
+        <TextInput
+          value={searchParams.get('filter') || ''}
+          onChange={(event) => {
+            let filter = event.target.value
+            if (filter) {
+              setSearchParams({ filter })
+            } else {
+              setSearchParams({})
+            }
+          }}
+        />
+        <Space h="md" />
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get('filter')
+            if (!filter) return true
+            let name = invoice.name.toLowerCase()
+            return name.startsWith(filter.toLowerCase())
+          })
+          .map((invoice) => (
+            <Navbar.Section>
+              <NavLink
+                style={({ isActive }) => {
+                  return {
+                    textDecorationLine: 'none',
+                  }
+                }}
+                to={`/invoices/${invoice.number}`}
+                key={invoice.number}
+              >
+                <UnstyledButton
+                  sx={(theme) => ({
+                    display: 'block',
+                    width: '100%',
+                    padding: theme.spacing.xs,
+                    borderRadius: theme.radius.sm,
+                    color:
+                      theme.colorScheme === 'dark'
+                        ? theme.colors.dark[0]
+                        : theme.black,
+
+                    '&:hover': {
+                      backgroundColor:
+                        theme.colorScheme === 'dark'
+                          ? theme.colors.dark[6]
+                          : theme.colors.gray[0],
+                    },
+                  })}
+                >
+                  <Text>{invoice.name}</Text>
+                </UnstyledButton>
+              </NavLink>
+            </Navbar.Section>
+          ))}
+      </Navbar>
       <Outlet />
     </div>
   )
